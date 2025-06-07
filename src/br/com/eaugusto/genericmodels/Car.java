941a87e1d2683efbb87108babd316dcd34168d9c
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-07 00:32:34 -0300

    Created Car Abstract Class

--- src/br/com/eaugusto/genericmodels/Car.java
+++ src/br/com/eaugusto/genericmodels/Car.java
@@ -3,10 +3,33 @@
  * @since Jun 6, 2025
  */
 
-
 package br.com.eaugusto.genericmodels;
 
 public abstract class Car {
+	private String model;
+	private String brand;
+
+	protected Car(String model, String brand) {
+		this.setModel(model);
+		this.setBrand(brand);
+	}
+
+	public abstract void showInformation();
+
+	public String getBrand() {
+		return brand;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setBrand(String brand) {
+		this.brand = brand;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
 
 }
-
