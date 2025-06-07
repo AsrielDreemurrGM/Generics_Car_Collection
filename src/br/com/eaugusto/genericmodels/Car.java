42a9feb3ca506475399512f32dc105f4d95a858a
Eduardo Augusto <eduardo.motta.980315@gmail.com>
2025-06-06 23:59:34 -0300

    Created Starting Class Files That Will Be Used
    - Created .gitignore;

---
+++ src/br/com/eaugusto/genericmodels/Car.java
@@ -0,0 +1,12 @@
+/**
+ * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
+ * @since Jun 6, 2025
+ */
+
+
+package br.com.eaugusto.genericmodels;
+
+public abstract class Car {
+
+}
+
---
+++ src/br/com/eaugusto/main/App.java
@@ -0,0 +1,14 @@
+/**
+ * @author Eduardo Augusto (https://github.com/AsrielDreemurrGM/)
+ * @since Jun 6, 2025
+ */
+
+package br.com.eaugusto.main;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+}
